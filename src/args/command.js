71747d8fad9a18ae038718bae7f9471b3c4bdc21
krins <krinskumar@gmail.com>
2024-10-10 23:31:49 -0400

    Contains all the changes for refactoring refactored-function
    extracted args functions
    
    better variable names

--- src/args/command.js
+++ src/args/command.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 import fs from "fs";
-import { validateArgs, getUrlBody, convertIntoMd, saveMd } from "./helper.js";
+import { updateState, getUrlBody, convertIntoMd, saveMd } from "./helper.js";
+import { validateArgs } from "./args.js";
 
 // Check if both input and output file names are provided
 let state = await validateArgs(process.argv);
 if (!state) {
   process.exit(1);
 }
+
+updateState(state);
 
 // add emogi
 process.stdout.write("Validations complete ✅\n");
@@ -18,12 +21,12 @@ let url = state.isInputFile
 
 // get the body of the url
 process.stdout.write("Getting the body from the url : " + url + " ");
-const body = await getUrlBody(url);
+const convertedBody = await getUrlBody(url);
 process.stdout.write("Body fetched successfully ✅\n");
 
 // convert the body into md
 process.stdout.write("Converting the body into markdown... ");
-const md = await convertIntoMd(body);
+const md = await convertIntoMd(convertedBody);
 process.stdout.write("\nConversion complete ✅\n");
 
 // save the md into the output file
--- src/args/helper.js
+++ src/args/helper.js
@@ -11,7 +11,6 @@ dotenv.config();
 // Get the current file path and directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const helpTxt = `Usage: scrappy [options] <inputFile> <outputFile> <url>
 
 Options:
@@ -40,141 +39,8 @@ let state = {
   stream: false,
 };
 
-// will validate the arguments provided and populate the state object.
-export async function validateArgs(args) {
-  let configOptions;
-  try {
-    const configFilePath = path.join(os.homedir(), ".scrappy.toml");
-    configOptions = await parseConfig(configFilePath);
-  } catch (error) {
-    // If file found but couldn't be parsed, exit
-    if (error.code !== "ENOENT") {
-      process.stderr.write(error.toString());
-      process.exit(1);
-    }
-  }
-
-  // check if the api key is proivded
-  if (args.includes("--api-key") || args.includes("-a")) {
-    let index;
-    if (args.includes("-a")) {
-      index = args.indexOf("-a");
-    } else {
-      index = args.indexOf("--api-key");
-    }
-    if (args.length <= index + 1) {
-      process.stderr.write("Please provide a valid api key");
-      process.exit(1);
-    }
-    process.env.GROQ_API_KEY = args[index + 1];
-    state.apiKey = args[index + 1];
-    // Write the API key to the .env file
-    const envFilePath = path.resolve(__dirname, "../..", ".env");
-    fs.appendFileSync(envFilePath, `GROQ_API_KEY=${state.apiKey}\n`, "utf8");
-
-    process.stdout.write("API key updated and written to .env file");
-    return false;
-  }
-
-  //  check if the version flag is passed.
-  if (args.includes("-v") || args.includes("--version")) {
-    process.stdout.write("Version 1.0.0");
-    return false;
-  }
-
-  //  check if the help flag is passed.
-  if (args.includes("-h") || args.includes("--help")) {
-    process.stdout.write(helpTxt);
-    return false;
-  }
-
-  process.stdout.write("Validating the arguments... ");
-
-  // Use URL CLI option if present
-  // If missing, use input file argument if present
-  // If missing, use URL config option if present
-  // If missing, use input file config option if present
-  // If missing, exit
-  if (args.includes("--url") || args.includes("-u")) {
-    let index;
-    if (args.includes("-u")) {
-      index = args.indexOf("-u");
-    } else {
-      index = args.indexOf("--url");
-    }
-
-    if (args.length <= index + 1) {
-      process.stderr.write("Please provide a valid url");
-      process.exit(1);
-    }
-    
-    state.url = args[index + 1];
-    state.isInputFile = false;
-  } else {
-    let [, , inputFile] = args; // Check if the input file is supplied as an argument
-    if (!inputFile) {
-      if (configOptions?.url) {
-        state.url = configOptions.url;
-      } else if (configOptions?.inputFile) {
-        state.inputFile = configOptions.inputFile;
-      } else {
-        process.stderr.write("Please provide the input file");
-        process.exit(1);
-      }
-    }
-
-    // check if the input file exists
-    if (!fs.existsSync(inputFile)) {
-      console.log(inputFile);
-      process.stderr.write("The input file does not exist");
-      process.exit(1);
-    }
-    state.inputFile = inputFile;
-    state.isInputFile = true;
-  }
-
-  // check if the outfile file is provided
-  if (args.includes("-o") || args.includes("--output")) {
-    let index;
-    if (args.includes("-o")) {
-      index = args.indexOf("-o");
-    } else {
-      index = args.indexOf("--output");
-    }
-    if (index === -1) {
-      index = args.indexOf("--output");
-      if (args.length <= index + 1) {
-        process.stderr.write("Please provide the output file");
-        process.exit(1);
-      }
-    }
-    state.outputFile = args[index + 1];
-  } else if (configOptions?.outputFile) {
-    state.outputFile = configOptions.outputFile;
-  }
-
-  // check if the token usage flag is passed
-  if (args.includes("-t") || args.includes("--token-usage")) {
-    let index;
-    if (args.includes("-t")) {
-      index = args.indexOf("-t");
-    } else {
-      index = args.indexOf("--token-usage");
-    }
-
-    state.tokenUsage = true;
-  } else if (configOptions?.tokenUsage) {
-    state.tokenUsage = configOptions.tokenUsage;
-  }
-
-  // check if the stream flag is passed --stream/-s
-  if (args.includes("--stream") || args.includes("-s")) {
-    state.stream = true;
-  } else if (configOptions?.stream) {
-    state.stream = configOptions.stream;
-  }
-
-  return state;
+export function updateState(newState) {
+  state = { ...state, ...newState };
 }
 
 // gets the body of the url provided
@@ -271,10 +137,18 @@ export function saveMd(md) {
  * @param {string} configFilePath Path to .toml config file
  * @returns {{ url: string | undefined, inputFile: string | undefined, outputFile: string | undefined, tokenUsage: boolean | undefined, stream: boolean | undefined }} An object containing the parsed options
  */
-export async function parseConfig(configFilePath) {
-  const configfileContent = await fsP.readFile(configFilePath, {
-    encoding: "utf8",
-  });
-  const configOptions = TOML.parse(configfileContent);
-  return configOptions;
+export async function getConfigOptions() {
+  try {
+    const configFilePath = path.join(os.homedir(), ".scrappy.toml");
+    const configfileContent = await fsP.readFile(configFilePath, {
+      encoding: "utf8",
+    });
+    return TOML.parse(configfileContent);
+  } catch (error) {
+    // If file found but couldn't be parsed, exit
+    if (error.code !== "ENOENT") {
+      process.stderr.write(error.toString());
+      process.exit(1);
+    }
+  }
 }
